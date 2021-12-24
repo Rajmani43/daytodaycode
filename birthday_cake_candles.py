4d15debcbe7510f6f6f2c64c3780126401121c16
RAJ MANI <mitresh678@gmail.com>
2021-12-24 23:35:18 +0530

    Program to find number of same largest element in an array

---
+++ birthday_cake_candles.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    sum = 0
+    max = candles[0]
+    for i in range(len(candles)):
+        if max < candles[i]:
+            max = candles[i]
+    for i in range(len(candles)):
+        if candles[i] == max:
+            sum += 1
+    return sum
+            
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
